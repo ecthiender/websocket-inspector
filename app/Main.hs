cebee336b6e18f444ad93faecef6dc88239003aa
Anon Ray <rayanon004@gmail.com>
2020-07-24 06:10:18 +0530

    error handling and connection status
      - proper error handling. TODO: there are more uncaught exceptions.
      check them out.
      - display connection status
      - minor styling updates

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,77 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TypeApplications  #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Main where
 
 import           Brick
 import           Brick.BChan
 import           Control.Concurrent (forkIO, threadDelay)
+import           Control.Exception  (IOException, SomeException, catch, try)
 import           Control.Monad      (forever, void)
+import           Data.Maybe         (fromMaybe)
 import           Data.Time.Clock
-import           Lib
+import           Network.URI
 import           Options.Generic    (Generic)
 import           System.Environment (getArgs)
+import           Text.Read          (readMaybe)
 
 import qualified Data.Text          as T
 import qualified Network.WebSockets as WS
 import qualified Options.Generic    as Options
+import qualified Wuss               as WS
+
+import           Lib
 
 data AppOptions
   = AppOptions
-  { --url :: String Options.<?> "URL Format: ws://example.com:8080/some_path"
-    host :: String
-  , port :: Int
-  , path :: String
+  { url :: String
   } deriving (Show, Generic)
 
 instance Options.ParseRecord AppOptions
 
 main :: IO ()
 main = do
-  AppOptions{..} <- Options.getRecord "websocket-inspector"
+  options <- Options.getRecord "websocket-inspector"
+  (scheme, host, port, path) <- either error return $ parseURL $ url options
+
   brickChan <- newBChan 100
   clientChan <- newBChan 100
 
-  forkIO $ WS.runClient host port path $ websocketClient brickChan clientChan
-  runUI brickChan clientChan
+  -- Start the websocket system in a separate thread; as the main thread will run the UI
+  forkIO $
+    runWSClient scheme host port path (websocketClient brickChan clientChan)
+      `catch` (writeBChan brickChan . WIEConnectionUpdate . CSError . T.pack . show @IOException)
 
-  -- WS.runSecureClient "gateway.discord.gg" 443 "/?v=6&encoding=json" $ \conn -> do
+  -- run the UI on the main thread
+  runUI brickChan clientChan
+  where
+    runWSClient scheme host port path app = case scheme of
+      "ws:"  -> WS.runClient host port path app
+      "wss:" -> WS.runSecureClient host (fromIntegral port) path app
+
+parseURL :: String -> Either String (String, String, Int, String)
+parseURL url' =
+  case parseURI url' of
+    Nothing -> Left "Invalid URI. Format: ws://example.com:8080/somepath"
+    Just URI{..} ->
+      let host = maybe (Left "Invalid hostname") (Right . uriRegName) uriAuthority
+          port = fromMaybe (defaultPort uriScheme) $ readMaybe =<< fmap (drop 1 . uriPort) uriAuthority
+          path = uriPath <> uriQuery <> uriFragment
+      in (,,,) <$> pure uriScheme <*> host <*> pure port <*> pure path
+  where
+    defaultPort scheme = case scheme of
+      "ws:"  -> 80
+      "wss:" -> 443
 
 websocketClient :: BChan WIEvent -> BChan Options.Text -> WS.Connection -> IO b
 websocketClient brickChan clientChan conn = do
+  writeBChan brickChan (WIEConnectionUpdate CSConnected)
+
   void $ forkIO $ forever $ do
     msg <- readBChan clientChan
     curTime <- getCurrentTime
@@ -50,6 +79,8 @@ websocketClient brickChan clientChan conn = do
     WS.sendTextData conn msg
 
   forever $ do
-    msg <- WS.receiveData conn
+    eitherMsg <- try @IOException $ WS.receiveData conn
     curTime <- getCurrentTime
-    writeBChan brickChan (WIENewServerMsg msg curTime)
+    case eitherMsg of
+      Left e    -> writeBChan brickChan (WIEConnectionUpdate CSClosed)
+      Right msg -> writeBChan brickChan (WIENewServerMsg msg curTime)
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,14 @@
 module Lib
   ( runUI
   , WIEvent (..)
+  , ConnectionStatus (..)
   ) where
 
 import           Brick
+import           Brick.AttrMap              (AttrMap, attrMap)
 import           Brick.BChan
 import           Brick.Types                (handleEventLensed)
+import           Brick.Util                 (fg, on)
 import           Brick.Widgets.Border
 import           Brick.Widgets.Border.Style
 import           Brick.Widgets.Center
@@ -19,6 +22,9 @@ import           Control.Monad.IO.Class
 import           Data.Text                  (Text)
 import           Data.Text.Zipper
 import           Data.Time.Clock
+import           Data.Time.Format
+import           Graphics.Vty               (Attr, black, blue, cyan, green, red, white, withURL,
+                                             yellow)
 import           Graphics.Vty.Attributes    (defAttr)
 import           Lens.Micro
 import           Lens.Micro.TH
@@ -28,14 +34,26 @@ import qualified Data.Text                  as T
 import qualified Data.Vector                as Vec
 import qualified Graphics.Vty               as Vty
 
-type ResourceName = Text
+data ResourceName
+  = RNMessageEditor
+  | RNServerBox
+  | RNClientBox
+  deriving (Show, Eq, Ord)
+
+data ConnectionStatus
+  = CSConnecting
+  | CSError !Text
+  | CSConnected
+  | CSClosed
+  deriving (Show)
 
 -- | The state we want to maintain
 data AppState
   = AppState
-  { _asClientMessages :: !(BrickL.List ResourceName (UTCTime, Text))
-  , _asServerMessages :: !(BrickL.List ResourceName (UTCTime, Text))
-  , _asMessageEditor  :: !(Editor Text ResourceName)
+  { _asClientMessages   :: !(BrickL.List ResourceName (UTCTime, Text))
+  , _asServerMessages   :: !(BrickL.List ResourceName (UTCTime, Text))
+  , _asMessageEditor    :: !(Editor Text ResourceName)
+  , _asConnectionStatus :: !ConnectionStatus
   } deriving (Show)
 
 makeLenses 'AppState
@@ -44,32 +62,50 @@ makeLenses 'AppState
 data WIEvent
   = WIENewServerMsg !Text !UTCTime
   | WIENewClientMsg !Text !UTCTime
+  | WIEConnectionUpdate !ConnectionStatus
   deriving (Show)
 
-drawMessageBox :: Text -> BrickL.List ResourceName (UTCTime, Text) -> Widget ResourceName
-drawMessageBox label msgs =
+drawMessageBox :: Text -> AttrName -> BrickL.List ResourceName (UTCTime, Text) -> Widget ResourceName
+drawMessageBox label attrName msgs =
   borderWithLabel (txt label) $ BrickL.renderList renderMsg False msgs
   where
-    renderMsg _selected (time, msg) = txtWrap $ "[" <> (T.pack $ show time) <> "] " <> msg
-    vBoxFill ws = vBox $ ws <> [fill ' ']
+    -- TODO: strip message of escape sequence, tab and carriage return. see
+    -- https://hackage.haskell.org/package/brick-0.55/docs/Brick-Widgets-Core.html#v:txt
+    renderMsg _selected (time, msg) =
+      withAttr attrName (txt "> ") <+> txtWrap (timestamp time <> msg)
+    timestamp t =
+      let fmtTime = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" t
+      in "[" <> T.pack fmtTime <> "] "
 
 drawReadingBox :: AppState -> Widget ResourceName
-drawReadingBox (AppState clientMsgs serverMsgs _) =
-  withBorderStyle unicode $
-  (    center (drawMessageBox "Messages from Server" serverMsgs)
-   <+> center (drawMessageBox "Messages sent by you (client)" clientMsgs)
-  )
+drawReadingBox (AppState clientMsgs serverMsgs _ _) =
+  center (drawMessageBox "Messages from Server" tsServer serverMsgs)
+  <+> center (drawMessageBox "Messages sent by you (client)" tsClient clientMsgs)
 
 drawInputBox :: AppState -> Widget ResourceName
 drawInputBox state =
   withBorderStyle unicode $ borderWithLabel (txt "Message Editor") $
   txt "> " <+> renderEditor (txt . T.unlines) True (state ^. asMessageEditor)
 
+drawStats :: AppState -> Widget ResourceName
+drawStats AppState{..} =
+  let status = case _asConnectionStatus of
+        CSConnecting -> "Connecting"
+        CSError err  -> "Error: " <> err
+        CSConnected  -> "Connected"
+        CSClosed     -> "Connection closed by server"
+  in withAttr statusHud $ withBorderStyle unicode $
+     borderWithLabel (txt "Status") $ txt status
+
+drawBottomControl :: AppState -> Widget ResourceName
+drawBottomControl appState = drawInputBox appState <+> drawStats appState
+
 footerHelp :: Widget ResourceName
-footerHelp = txt "HELP: **ESC**: quit/exit | **ENTER**: Send message"
+footerHelp = txt "HELP: **ESC**: quit/exit | **ENTER**: Send message | Timestamps are in UTC."
 
 drawUI :: AppState -> [Widget ResourceName]
-drawUI appState = [drawReadingBox appState <=> drawInputBox appState <=> footerHelp]
+drawUI appState =
+  [drawReadingBox appState <=> drawBottomControl appState <=> footerHelp]
 
 handleEvent :: BChan Text -> AppState -> BrickEvent ResourceName WIEvent -> EventM ResourceName (Next AppState)
 handleEvent clientChan state@AppState{..} ev = case ev of
@@ -79,6 +115,7 @@ handleEvent clientChan state@AppState{..} ev = case ev of
                            in continue $ state { _asClientMessages = newList }
     WIENewServerMsg m t -> let newList = insertNewMessage (t,m) _asServerMessages
                            in continue $ state { _asServerMessages = newList }
+    WIEConnectionUpdate status -> continue $ state { _asConnectionStatus = status }
   -- handle Vty key events
   VtyEvent vev -> case vev of
     Vty.EvKey Vty.KEsc []   -> halt state
@@ -117,15 +154,35 @@ mkApp clientChan =
   App { appDraw = drawUI
       , appHandleEvent = handleEvent clientChan
       , appStartEvent = return
-      , appAttrMap = const $ attrMap defAttr []
+      , appAttrMap = styleMap
       , appChooseCursor = showFirstCursor
       }
 
+-- globalDefault :: Attr
+-- globalDefault = white `on` black
+
+tsServer = "ts-server"
+tsClient = "ts-client"
+statusHud = "connection-status"
+
+styleMap :: AppState -> AttrMap
+styleMap state = attrMap defAttr
+  [ (tsServer, fg blue)
+  , (tsClient, fg green)
+  , (statusHud, mkStatusHudStyle)
+  ]
+  where
+    mkStatusHudStyle = case state ^. asConnectionStatus of
+      CSConnecting -> fg yellow
+      CSError _    -> fg red
+      CSClosed     -> fg red
+      CSConnected  -> fg green
+
 runUI :: BChan WIEvent -> BChan Text -> IO ()
 runUI brickChan clientChan = do
-  let initialState = AppState clientList serverList (editor "edit-box" (Just 1) "")
-      clientList = BrickL.list "client-list" Vec.empty 1
-      serverList = BrickL.list "server-list" Vec.empty 1
+  let initialState = AppState clientList serverList (editor RNMessageEditor (Just 1) "") CSConnecting
+      clientList = BrickL.list RNClientBox Vec.empty 1
+      serverList = BrickL.list RNServerBox Vec.empty 1
       buildVty = Vty.mkVty Vty.defaultConfig
   initialVty <- buildVty
   void $ customMain initialVty buildVty (Just brickChan) (mkApp clientChan) initialState
